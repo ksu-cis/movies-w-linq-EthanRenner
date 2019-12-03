a6382b2e0740c6011923041f505f385c43f9a1c4
Ethan Renner <eprenner@win.cis.ksu.edu>
2019-12-03 14:24:10 -0600

    Finished in class activities for 12/3. Changed sorting, filtering to use LINQ.

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -9,7 +9,7 @@ namespace Movies.Pages
 {
     public class IndexModel : PageModel
     {
-        public List<Movie> Movies;
+        public IEnumerable<Movie> Movies;
 
         [BindProperty]
         public string search { get; set; }
@@ -23,11 +23,13 @@ namespace Movies.Pages
         [BindProperty]
         public float? maxIMDB { get; set; }
 
+        [BindProperty]
+        public string sort { get; set; }
 
 
         public void OnGet()
         {
-            Movies = MovieDatabase.All;
+            Movies = MovieDatabase.All.OrderBy(movie => movie.Title);
         }
 
         public void OnPost()
@@ -36,20 +38,51 @@ namespace Movies.Pages
 
             if (search != null)
             {
-                Movies = MovieDatabase.Search(Movies, search);
+                //Movies = MovieDatabase.Search(Movies, search);
+                Movies = Movies.Where(movie => 
+                    movie.Title.Contains(search, StringComparison.OrdinalIgnoreCase) || 
+                    (movie.Director != null && movie.Director.Contains(search, StringComparison.OrdinalIgnoreCase)));
             }
 
             if(mpaa.Count != 0)
             {
-                Movies = MovieDatabase.FilterByMPAA(Movies, mpaa);
+                //Movies = MovieDatabase.FilterByMPAA(Movies, mpaa);
+                Movies = Movies.Where(movie => mpaa.Contains(movie.MPAA_Rating));
             }
 
             if(minIMDB != null)
             {
-                Movies = MovieDatabase.FilterByMinIMDB(Movies, (float)minIMDB);
+                //Movies = MovieDatabase.FilterByMinIMDB(Movies, (float)minIMDB);
+                Movies = Movies.Where(movie => movie.IMDB_Rating != null && minIMDB <= movie.IMDB_Rating);
             }
 
-            
+            if (maxIMDB != null)
+            {
+                //Movies = MovieDatabase.FilterByMinIMDB(Movies, (float)minIMDB);
+                Movies = Movies.Where(movie => movie.IMDB_Rating != null && maxIMDB >= movie.IMDB_Rating);
+            }
+
+            if (sort != null)
+            {
+                switch (sort)
+                {
+                    case "director":
+                        Movies = Movies.Where(movie => movie.Director != null).OrderBy(movie => movie.Director);
+                        break;
+                    case "year":
+                        Movies = Movies.Where(movie => movie.Release_Year != null).OrderBy(movie => movie.Release_Year);
+                        break;
+                    case "imdb":
+                        Movies = Movies.Where(movie => movie.IMDB_Rating != null).OrderBy(movie => movie.IMDB_Rating);
+                        break;
+                    case "rt":
+                        Movies = Movies.Where(movie=>movie.Rotten_Tomatoes_Rating != null).OrderBy(movie => movie.Rotten_Tomatoes_Rating);
+                        break;
+                    default:
+                        Movies = Movies.OrderBy(movie => movie.Title);
+                        break;
+                }
+            }
         }
     }
 }
